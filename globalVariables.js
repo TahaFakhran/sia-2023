4b23388d42466fb4c61a22120c315e042edb10f8
Anthooons <anthonyhom@hotmail.com>
2023-05-06 20:29:48 +0200

    des changements

--- globalVariables.js
+++ globalVariables.js
@@ -20,6 +20,38 @@
   }
 ]
 
+let ouvrages_list = [
+                {
+                id:1,
+                name:"Tunnel sous la manche",
+                description:"Le Tunnel sous la Manche mesure 50,45 kilomètres de long, il a débuté le 15 décembre 1987 et a été achevé le 10 décembre 1993.Il s'agit d'un tunnel ferroviaire qui relie la France et l'Angleterre",
+                pays:"France/Angleterre"
+              },{
+                id:2,
+                name:"Tunnel du fréjus",
+                description:"Le tunnel routier du Fréjus, mis en service en 1980, long de 12,87 km, se situe à la jonction entre deux grands axes d'échanges régionaux et internationaux reliant la France et l'Italie.",
+                pays:"France"
+              },
+              {
+                id:3,
+                name:"Tunnel du mont blanc",
+                description:"Ce tunnel alpin a été ouvert à la circulation le 19 juillet 1965 et il fut inauguré par Charles de Gaulle alors président français de l'époque. Lors de son ouverture, ce fut le tunnel routier le plus long du monde.",
+                pays:"France"
+              },
+              {
+                id:4,
+                name:"Tunnel du simplon",
+                description:"Ce tunnel ferroviaire relie l'Italie à la Suisse. Il s'agit là encore d'un tunnel ferroviaire de 19,823 kilomètres de long. Il a été achevé en 1905.",
+                pays:"Suisse"
+              },
+              {
+                id:5,
+                name:"Tunnel de base des Apennins",
+                description:"Le tunnel de base des Apennins mesure 18,5 kilomètres de long et a été terminé en 1934.",
+                pays:"Italie"
+              }
+            ]
+
 let Utilisateurs = [
   ["ahere", "Hère", "Axel", "axel.here@betuf.fr", "abcde", "Directeur", ""],
   [
@@ -110,7 +142,7 @@ let Utilisateurs = [
     "alphonse.toudroit@betuf.fr",
     "abcde",
     "Chargé de missions",
-    "",
+    "Géologie, Géothermie",
   ],
   [
     "apartemental",
@@ -119,7 +151,7 @@ let Utilisateurs = [
     "amedee.partemental@betuf.fr",
     "abcde",
     "Chargé de missions",
-    "",
+    "Constructio, Marchés, Exécution",
   ],
   [
     "afrastructure",
@@ -128,7 +160,7 @@ let Utilisateurs = [
     "alain.frastructure@betuf.fr",
     "abcde",
     "Chargé de missions",
-    "",
+    "Exploitation",
   ],
   [
     "vdevaux",
@@ -137,7 +169,7 @@ let Utilisateurs = [
     "valerie.devaux@betuf.fr",
     "abcde",
     "Chargé de missions",
-    "",
+    "Equipements électriques et gestion",
   ],
   [
     "ajohn",
@@ -146,7 +178,7 @@ let Utilisateurs = [
     "aline.john@betuf.fr",
     "abcde",
     "Chargé de missions",
-    "",
+    "Equipements électriques et gestion",
   ],
   [
     "lrection",
@@ -155,7 +187,7 @@ let Utilisateurs = [
     "lydie.rection@betuf.fr",
     "abcde",
     "Chargé de missions",
-    "",
+    "Sécurité",
   ],
   [
     "jdoe",
@@ -167,3 +199,69 @@ let Utilisateurs = [
     "Sécurité",
   ],
 ];
+
+
+eventDB = [
+  {
+    id:"hadw8hiaua",
+    title: 'Check up',
+    start: '2023-05-01',
+    persone:"jdoe",
+    url: 'Modifier.html?id=' + "hadw8hiaua",
+    pole:"Sécurité"
+  },
+  {
+    id:"a9suh9as",
+    title: 'Traveaux',
+    start: '2023-5-07',
+    end: '2023-05-10',
+    persone:"atoutdroit",
+    url: 'Modifier.html?id=' + "a9suh9as",
+    pole:"Géologie, Géothermie",
+  },
+
+  {
+    id: "ashhc8sc",
+    title: 'Conference',
+    start: '2023-05-09T16:00:00',
+    persone:"rtaurout",
+    url: 'Modifier.html?id=' + "ashhc8sc",
+    pole:"Equipements électriques et gestion"
+  },
+
+  {
+    id: "awdadadaaw",
+    title: 'Conference',
+    start: '2023-05-16T16:00:00',
+    persone:"ahere",
+    url: 'Modifier.html?id=' + "awdadadaaw",
+    pole:"",
+  },
+  {
+    id: "awdadadaaw",
+    title: 'Traveaux',
+    start: '2023-05-11',
+    end: '2023-05-13',
+    persone:"lrection",
+    url: 'Modifier.html?id=' + "a9suh9as",
+    pole:"Sécurité",
+  },
+  {
+    id: "drgdrgdr",
+    title: 'Traveaux',
+    start: '2023-05-12T10:30:00', 
+    end: '2023-05-12T12:30:00',
+    persone:"atoutdroit",
+    url: 'Modifier.html?id=' + "drgdrgdr",
+    pole:"Géologie, Géothermie",
+  },
+  {
+    id: "hgfdsawd",
+    title: 'Check up',
+    start: '2023-05-12T12:00:00',
+    persone:"atoutdroit",
+    url: 'Modifier.html?id=' + "hgfdsawd",
+    pole:"Géologie, Géothermie",
+  },
+
+]
