f97c19cc32db8d3dab5e3e584e4b942486eb89a0
TahaFakhran <tahafakhran@gmail.com>
2023-05-06 14:26:54 +0200

    deleted webflow file

--- sia.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", (event) => {
-  const calendarElement = document.querySelector('[data-element="calendar"]');
-  if (!calendarElement) return;
-
-  console.log(calendarElement);
-});
