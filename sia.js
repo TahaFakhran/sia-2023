b11f5746e51e97c92a3c17634685aab1cba7e74c
TahaFakhran <tahafakhran@gmail.com>
2023-05-06 13:57:20 +0200

    last try

--- sia.js
+++ sia.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", (event) => {
-  const calendarElement =
-    document.querySelector < HTMLElement > '[data-element="calendar"]';
+  const calendarElement = document.querySelector('[data-element="calendar"]');
   if (!calendarElement) return;
 
   console.log(calendarElement);
