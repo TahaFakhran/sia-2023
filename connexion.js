763e351ef5f32aad3c83aa335cbf6bd47d26bf67
TahaFakhran <tahafakhran@gmail.com>
2023-05-08 02:54:14 +0200

    all done

--- connexion.js
+++ connexion.js
@@ -1,95 +1,222 @@
 // Base de données
 var Utilisateurs = [
-    ['ahere', 'Hère', 'Axel', 'axel.here@betuf.fr', 'abcde', 'Directeur', ''],
-    ['vroule', 'Roule', 'Vanessa', 'vanessa.roule@betuf.fr', 'abcdef', 'Directeur recherche', ''],
-    ['vsunnick', 'Sunnick', 'Vincent', 'vincent.sunnick@betuf.fr', 'abcdefg', 'Secrétaire général', ''],
-    ['gtaie', 'Taie', 'Gilbert', 'gilbert.taie@betuf.fr', 'abcdefgh', 'Chef de pôle', 'Constructio, Marchés, Exécution'],
-    ['jvoilbout', 'Voilbout', 'Jean', 'jean.voilbout@betuf.fr', 'abcdefghi', 'Chef de pôle', 'Géologie, Géothermie'],
-    ['lvragedar', 'Vragedar', 'Louis', 'louis.vragedar@betuf.fr', 'abcdefghij', 'Chef de pôle', 'Matériaux, structure'],
-    ['athinerairebis', 'Thinérairebis', 'Ali', 'ali.thinerairebis@betuf.fr', 'kkkk', 'Chef de pôle', 'Ventilation et Environnement'],
-    ['adoublesens', 'Doublesens', 'Angela', 'angela.doublesens@betuf.fr', 'jjjj', 'Chef de pôle', 'Sécurité'],
-    ['ofour', 'Four', 'Oscar', 'oscar.four@betuf.fr', 'iiii', 'Chef de pôle', 'Exploitation'],
-    ['rtaurout', 'Taurout', 'Roméo', 'romeo.taurout@betuf.fr', 'hhhh', 'Chef de pôle', 'Equipements électriques et gestion'],
-    ['atoutdroit', 'Toudroit', 'Alphonse', 'alphonse.toudroit@betuf.fr', 'gggg', 'Chargé de missions', ''],
-    ['apartemental', 'Partemental', 'Amédée', 'amedee.partemental@betuf.fr', 'ffff', 'Chargé de missions', ''],
-    ['afrastructure', 'Frastructure', 'Alain', 'alain.frastructure@betuf.fr', 'eeee', 'Chargé de missions', ''],
-    ['vdevaux', 'Devaux', 'Valérie', 'valerie.devaux@betuf.fr', 'dddd', 'Chargé de missions', ''],
-    ['ajohn', 'John', 'Aline', 'aline.john@betuf.fr', 'cccc', 'Chargé de missions', ''],
-    ['lrection', 'Rection', 'Lydie', 'lydie.rection@betuf.fr', 'bbbb', 'Chargé de missions', ''],
-    ['jdoe', 'Doe', 'Jane', 'jane.doe@betuf.fr', 'aaaa', 'Technicien', 'Sécurité']
-]
+  ["ahere", "Hère", "Axel", "axel.here@betuf.fr", "abcde", "Directeur", ""],
+  [
+    "vroule",
+    "Roule",
+    "Vanessa",
+    "vanessa.roule@betuf.fr",
+    "abcdef",
+    "Directeur recherche",
+    "",
+  ],
+  [
+    "vsunnick",
+    "Sunnick",
+    "Vincent",
+    "vincent.sunnick@betuf.fr",
+    "abcdefg",
+    "Secrétaire général",
+    "",
+  ],
+  [
+    "gtaie",
+    "Taie",
+    "Gilbert",
+    "gilbert.taie@betuf.fr",
+    "abcdefgh",
+    "Chef de pôle",
+    "Constructio, Marchés, Exécution",
+  ],
+  [
+    "jvoilbout",
+    "Voilbout",
+    "Jean",
+    "jean.voilbout@betuf.fr",
+    "abcdefghi",
+    "Chef de pôle",
+    "Géologie, Géothermie",
+  ],
+  [
+    "lvragedar",
+    "Vragedar",
+    "Louis",
+    "louis.vragedar@betuf.fr",
+    "abcdefghij",
+    "Chef de pôle",
+    "Matériaux, structure",
+  ],
+  [
+    "athinerairebis",
+    "Thinérairebis",
+    "Ali",
+    "ali.thinerairebis@betuf.fr",
+    "kkkk",
+    "Chef de pôle",
+    "Ventilation et Environnement",
+  ],
+  [
+    "adoublesens",
+    "Doublesens",
+    "Angela",
+    "angela.doublesens@betuf.fr",
+    "jjjj",
+    "Chef de pôle",
+    "Sécurité",
+  ],
+  [
+    "ofour",
+    "Four",
+    "Oscar",
+    "oscar.four@betuf.fr",
+    "iiii",
+    "Chef de pôle",
+    "Exploitation",
+  ],
+  [
+    "rtaurout",
+    "Taurout",
+    "Roméo",
+    "romeo.taurout@betuf.fr",
+    "hhhh",
+    "Chef de pôle",
+    "Equipements électriques et gestion",
+  ],
+  [
+    "atoutdroit",
+    "Toudroit",
+    "Alphonse",
+    "alphonse.toudroit@betuf.fr",
+    "gggg",
+    "Chargé de missions",
+    "",
+  ],
+  [
+    "apartemental",
+    "Partemental",
+    "Amédée",
+    "amedee.partemental@betuf.fr",
+    "ffff",
+    "Chargé de missions",
+    "",
+  ],
+  [
+    "afrastructure",
+    "Frastructure",
+    "Alain",
+    "alain.frastructure@betuf.fr",
+    "eeee",
+    "Chargé de missions",
+    "",
+  ],
+  [
+    "vdevaux",
+    "Devaux",
+    "Valérie",
+    "valerie.devaux@betuf.fr",
+    "dddd",
+    "Chargé de missions",
+    "",
+  ],
+  [
+    "ajohn",
+    "John",
+    "Aline",
+    "aline.john@betuf.fr",
+    "cccc",
+    "Chargé de missions",
+    "",
+  ],
+  [
+    "lrection",
+    "Rection",
+    "Lydie",
+    "lydie.rection@betuf.fr",
+    "bbbb",
+    "Chargé de missions",
+    "",
+  ],
+  [
+    "jdoe",
+    "Doe",
+    "Jane",
+    "jane.doe@betuf.fr",
+    "aaaa",
+    "Technicien",
+    "Sécurité",
+  ],
+];
 
 // let Events = [];
 
-ids = []
-mdps = []
+ids = [];
+mdps = [];
 
 var indice_co;
-for (let i = 0 ; i < Utilisateurs.length ; i++){
-    ids.push(Utilisateurs[i][0]);
-    mdps.push(Utilisateurs[i][4]);
+for (let i = 0; i < Utilisateurs.length; i++) {
+  ids.push(Utilisateurs[i][0]);
+  mdps.push(Utilisateurs[i][4]);
 }
 
-function login(){
-    open('login.html');
+function login() {
+  open("login.html");
 }
 
 if (sessionStorage.getItem("indice_co")) {
-    // Restore the contents of the text field
-    indice_co = sessionStorage.getItem("indice_co");
-  }
+  // Restore the contents of the text field
+  indice_co = sessionStorage.getItem("indice_co");
+}
 
-function connexion(){
-    var form = document.forms['form'];
-    id = document.form.user_name.value;
-    mdp = document.form.user_mdp.value;
-    if ((ids.indexOf(id) == mdps.indexOf(mdp)) & ids.indexOf(id) != -1){
-        indice_co = ids.indexOf(id);
-        sessionStorage.setItem('indice_co', indice_co);
-        open('home2.html');
-    }else{
-        alert("Identifiant ou mot de passe inconnu");
-    }
+function connexion() {
+  var form = document.forms["form"];
+  id = document.form.user_name.value;
+  mdp = document.form.user_mdp.value;
+  if ((ids.indexOf(id) == mdps.indexOf(mdp)) & (ids.indexOf(id) != -1)) {
+    indice_co = ids.indexOf(id);
+    sessionStorage.setItem("indice_co", indice_co);
+    open("home2.html");
+  } else {
+    alert("Identifiant ou mot de passe inconnu");
+  }
 }
 
 var nom = Utilisateurs[indice_co][1];
 var prenom = Utilisateurs[indice_co][2];
 
-
 // AUTORISATION DE CRÉER, SUPPRIMER OU MODIFIER DES EVENTS
 var auto_totale = false;
 var auto_partielle = false;
 var role = Utilisateurs[indice_co][5];
-if (role.indexOf('Directeur') != -1){
-    auto_totale = true;
+if (role.indexOf("Directeur") != -1) {
+  auto_totale = true;
+  auto_partielle = true;
+} else {
+  if (role.indexOf("Chef") != -1) {
     auto_partielle = true;
-}else{
-    if (role.indexOf('Chef') != -1){
-        auto_partielle = true;
-    }
+  }
 }
 
-function openAgenda(){
-    if (auto_totale){
-        open('calandarG.html');
-    }else{
-        if (auto_partielle){
-            open('calandarC.html');
-        }else{
-            open('calandarM.html');
-        }
+function openAgenda() {
+  if (auto_totale) {
+    open("calandarG.html");
+  } else {
+    if (auto_partielle) {
+      open("calandarC.html");
+    } else {
+      open("calandarM.html");
     }
+  }
 }
 
-function ajouter(){
-    auto_totale = autorisation(indice_co)[0]
-    auto_partielle = autorisation(indice_co)[1]
-    
-    if (auto_partielle){
-        open('ajouter.html');
-    }else{
-        alert("Vous n'êtes pas autorisé.e à aujouter des événements");
-    }
+function ajouter() {
+  auto_totale = autorisation(indice_co)[0];
+  auto_partielle = autorisation(indice_co)[1];
+
+  if (auto_partielle) {
+    open("ajouter.html");
+  } else {
+    alert("Vous n'êtes pas autorisé.e à aujouter des événements");
+  }
 }
 
 // function addEvt(){
@@ -111,11 +238,9 @@ function ajouter(){
 //             alert("Vous n'êtes pas autorisé à créer une réunion.")
 //         }
 //     }
-    
+
 // }
 
-
-
-function logout(){
-    open('home.html');
+function logout() {
+  open("home.html");
 }
--- globalVariables.js
+++ globalVariables.js
@@ -1,56 +1,63 @@
-  poles_list = [{
+poles_list = [
+  {
     name: "Sécurité",
-    id: 1
+    id: 1,
   },
   {
     name: "Equipements électriques et gestion",
-    id: 2
+    id: 2,
   },
   {
     name: "Exploitation",
-    id: 3
+    id: 3,
   },
   {
     name: "Géologie, Géothermie",
-    id: 4
+    id: 4,
   },
   {
-    name:"Constructio, Marchés, Exécution",
-    id:5
-  }
-]
+    name: "Constructio, Marchés, Exécution",
+    id: 5,
+  },
+];
 
 let ouvrages_list = [
-                {
-                id:1,
-                name:"Tunnel sous la manche",
-                description:"Le Tunnel sous la Manche mesure 50,45 kilomètres de long, il a débuté le 15 décembre 1987 et a été achevé le 10 décembre 1993.Il s'agit d'un tunnel ferroviaire qui relie la France et l'Angleterre",
-                pays:"France/Angleterre"
-              },{
-                id:2,
-                name:"Tunnel du fréjus",
-                description:"Le tunnel routier du Fréjus, mis en service en 1980, long de 12,87 km, se situe à la jonction entre deux grands axes d'échanges régionaux et internationaux reliant la France et l'Italie.",
-                pays:"France"
-              },
-              {
-                id:3,
-                name:"Tunnel du mont blanc",
-                description:"Ce tunnel alpin a été ouvert à la circulation le 19 juillet 1965 et il fut inauguré par Charles de Gaulle alors président français de l'époque. Lors de son ouverture, ce fut le tunnel routier le plus long du monde.",
-                pays:"France"
-              },
-              {
-                id:4,
-                name:"Tunnel du simplon",
-                description:"Ce tunnel ferroviaire relie l'Italie à la Suisse. Il s'agit là encore d'un tunnel ferroviaire de 19,823 kilomètres de long. Il a été achevé en 1905.",
-                pays:"Suisse"
-              },
-              {
-                id:5,
-                name:"Tunnel de base des Apennins",
-                description:"Le tunnel de base des Apennins mesure 18,5 kilomètres de long et a été terminé en 1934.",
-                pays:"Italie"
-              }
-            ]
+  {
+    id: 1,
+    name: "Tunnel sous la manche",
+    description:
+      "Le Tunnel sous la Manche mesure 50,45 kilomètres de long, il a débuté le 15 décembre 1987 et a été achevé le 10 décembre 1993.Il s'agit d'un tunnel ferroviaire qui relie la France et l'Angleterre",
+    pays: "France/Angleterre",
+  },
+  {
+    id: 2,
+    name: "Tunnel du fréjus",
+    description:
+      "Le tunnel routier du Fréjus, mis en service en 1980, long de 12,87 km, se situe à la jonction entre deux grands axes d'échanges régionaux et internationaux reliant la France et l'Italie.",
+    pays: "France",
+  },
+  {
+    id: 3,
+    name: "Tunnel du mont blanc",
+    description:
+      "Ce tunnel alpin a été ouvert à la circulation le 19 juillet 1965 et il fut inauguré par Charles de Gaulle alors président français de l'époque. Lors de son ouverture, ce fut le tunnel routier le plus long du monde.",
+    pays: "France",
+  },
+  {
+    id: 4,
+    name: "Tunnel du simplon",
+    description:
+      "Ce tunnel ferroviaire relie l'Italie à la Suisse. Il s'agit là encore d'un tunnel ferroviaire de 19,823 kilomètres de long. Il a été achevé en 1905.",
+    pays: "Suisse",
+  },
+  {
+    id: 5,
+    name: "Tunnel de base des Apennins",
+    description:
+      "Le tunnel de base des Apennins mesure 18,5 kilomètres de long et a été terminé en 1934.",
+    pays: "Italie",
+  },
+];
 
 let Utilisateurs2 = [
   ["ahere", "Hère", "Axel", "axel.here@betuf.fr", "abcde", "Directeur", ""],
@@ -200,75 +207,73 @@ let Utilisateurs2 = [
   ],
 ];
 
-
 eventDB = [
   {
-    id:"hadw8hiaua",
-    title: 'Check up',
-    start: '2023-05-01',
-    persone:"jdoe",
-    url: 'Modifier.html?id=' + "hadw8hiaua",
-    ouvrage:"Tunnel sous la manche",
-    pole:"Sécurité"
+    id: "hadw8hiaua",
+    title: "Check up",
+    start: "2023-05-01",
+    persone: "jdoe",
+    url: "Modifier.html?id=" + "hadw8hiaua",
+    ouvrage: "Tunnel sous la manche",
+    pole: "Sécurité",
   },
   {
-    id:"a9suh9as",
-    title: 'Traveaux',
-    start: '2023-5-07',
-    end: '2023-05-10',
-    persone:"atoutdroit",
-    url: 'Modifier.html?id=' + "a9suh9as",
-    ouvrage:"Tunnel sous la manche",
-    pole:"Géologie, Géothermie",
+    id: "a9suh9as",
+    title: "Traveaux",
+    start: "2023-5-07",
+    end: "2023-05-10",
+    persone: "atoutdroit",
+    url: "Modifier.html?id=" + "a9suh9as",
+    ouvrage: "Tunnel sous la manche",
+    pole: "Géologie, Géothermie",
   },
 
   {
     id: "ashhc8sc",
-    title: 'Conference',
-    start: '2023-05-09T16:00:00',
-    persone:"rtaurout",
-    url: 'Modifier.html?id=' + "ashhc8sc",
-    ouvrage:"Tunnel de base des Apennins",
-    pole:"Equipements électriques et gestion"
+    title: "Conference",
+    start: "2023-05-09T16:00:00",
+    persone: "rtaurout",
+    url: "Modifier.html?id=" + "ashhc8sc",
+    ouvrage: "Tunnel de base des Apennins",
+    pole: "Equipements électriques et gestion",
   },
 
   {
     id: "awdadadaaw",
-    title: 'Conference',
-    start: '2023-05-16T16:00:00',
-    persone:"ahere",
-    url: 'Modifier.html?id=' + "awdadadaaw",
-    ouvrage:"Tunnel de base des Apennins",
-    pole:"",
+    title: "Conference",
+    start: "2023-05-16T16:00:00",
+    persone: "ahere",
+    url: "Modifier.html?id=" + "awdadadaaw",
+    ouvrage: "Tunnel de base des Apennins",
+    pole: "",
   },
   {
     id: "awdadadaaw",
-    title: 'Traveaux',
-    start: '2023-05-11',
-    end: '2023-05-13',
-    persone:"lrection",
-    url: 'Modifier.html?id=' + "a9suh9as",
-    ouvrage:"Tunnel du simplon",
-    pole:"Sécurité",
+    title: "Traveaux",
+    start: "2023-05-11",
+    end: "2023-05-13",
+    persone: "lrection",
+    url: "Modifier.html?id=" + "a9suh9as",
+    ouvrage: "Tunnel du simplon",
+    pole: "Sécurité",
   },
   {
     id: "drgdrgdr",
-    title: 'Traveaux',
-    start: '2023-05-12T10:30:00', 
-    end: '2023-05-12T12:30:00',
-    persone:"atoutdroit",
-    url: 'Modifier.html?id=' + "drgdrgdr",
-    ouvrage:"Tunnel de base des Apennins",
-    pole:"Géologie, Géothermie",
+    title: "Traveaux",
+    start: "2023-05-12T10:30:00",
+    end: "2023-05-12T12:30:00",
+    persone: "atoutdroit",
+    url: "Modifier.html?id=" + "drgdrgdr",
+    ouvrage: "Tunnel de base des Apennins",
+    pole: "Géologie, Géothermie",
   },
   {
     id: "hgfdsawd",
-    title: 'Check up',
-    start: '2023-05-12T12:00:00',
-    persone:"atoutdroit",
-    url: 'Modifier.html?id=' + "hgfdsawd",
-    ouvrage:"Tunnel de base des Apennins",
-    pole:"Géologie, Géothermie",
+    title: "Check up",
+    start: "2023-05-12T12:00:00",
+    persone: "atoutdroit",
+    url: "Modifier.html?id=" + "hgfdsawd",
+    ouvrage: "Tunnel de base des Apennins",
+    pole: "Géologie, Géothermie",
   },
-
-]
+];
